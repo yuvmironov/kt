3a616aad60ee4fa9dc111e58aca862f5b70c5fe9
mfs <yuvmironov@gmail.com>
2019-11-05 22:41:05 +0300

    ferst commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<style lang="stylus">
+*
+  margin 0
+  padding 0
+  box-sizing border-box
+#app
+  background-image url('~@/assets/MainBG.jpg')
+  width 100vw
+  height 100vh
+</style>
