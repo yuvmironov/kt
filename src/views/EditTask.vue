941750478c7e77c0546f642237cb1cdaa8ac3c3e
mfs <yuvmironov@gmail.com>
2019-11-07 22:50:33 +0300

    Редактирование задачи

--- src/views/EditTask.vue
+++ src/views/EditTask.vue
@@ -1,9 +1,22 @@
 <template>
   <div class="Edit">
-    <input type="text" :value=findTask.task class="Edit-Input">
-    <button>Сохранить</button>
+    <p>{{ findTask.task }}</p>
+    <input type="text" v-model='task' class="Edit-Input" placeholder="Введите исправление">
+    <button v-on:click = 'saveTask'>Сохранить</button>
   </div>
 </template>
+
+<style lang="stylus" scoped>
+.Edit
+  width: 320px;
+  margin: 0 auto;
+  padding: 5px 5px;
+  background-color white
+  display flex
+  flex-direction column
+  &-Input
+    margin-bottom 5px
+</style>
 
 <script>
 import { db } from '../assets/db'
@@ -11,19 +24,37 @@ import { db } from '../assets/db'
 export default {
   data () {
     return {
-      listTasks: []
+      listTasks: [],
+      idTask: '',
+      task: ''
     }
   },
   firestore: {
     listTasks: db.collection('todolist')
   },
+  methods: {
+    setTask (idTask) {
+      this.idTask = idTask
+    },
+    saveTask () {
+      db.collection('todolist')
+        .doc(this.idTask)
+        .update({ id: this.idTask, task: this.task })
+        .then(() => {
+          this.$router.push('/')
+        })
+        .catch(e => {
+          alert('Не удалось обновить задачу')
+        })
+    }
+  },
   computed: {
     findTask () {
       let task = ''
-      console.log(this.listTasks)
       this.listTasks.forEach(item => {
         if (item.id === this.$route.params.id) {
           task = item
+          this.setTask(item.id)
         }
       })
       return task
